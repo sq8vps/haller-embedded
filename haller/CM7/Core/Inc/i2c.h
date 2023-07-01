fc5c9e18e43f75062d4126dd25d112f8d04bee41
dkot001 <dkot@student.agh.edu.pl>
2023-07-01 13:56:06 +0200

    Added general call

--- haller/CM7/Core/Inc/i2c.h
+++ haller/CM7/Core/Inc/i2c.h
@@ -11,6 +11,7 @@ extern "C" {
 void I2C1_Master_Init(void);
 void I2C1_Master_Write(uint8_t addr, uint32_t *msg, uint8_t size);
 void I2C1_Master_Read(uint8_t addr, uint32_t *buf, uint8_t size);
+void I2C1_GC(uint32_t *data, uint8_t size);
 
 #ifdef __cplusplus
 }
