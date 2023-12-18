2495ea9eb5bb0d1e71a29d2d137e57ebaf684621
Patryk Nalepka <aghpatryk@gmail.com>
2023-12-18 01:51:43 +0100

    added more pwm outputs

--- haller/CM7/Core/Inc/servo.h
+++ haller/CM7/Core/Inc/servo.h
@@ -1,11 +1,27 @@
 #ifndef INC_SERVO_H_
 #define INC_SERVO_H_
 
-#include "stm32f7xx_hal.h"
+#include <stdint.h>
 
-#define SERVO_DEFAULT_POSITION 75
-#define SERVO_MIN_POSITION 25
-#define SERVO_MAX_POSITION 125
+/* Pin configuration:
+ * TIM3:
+ * 	CH1 - PC6
+ * 	CH2 - PC7
+ * 	CH3 - PC8
+ * 	CH4 - PC9
+ * TIM4:
+ * 	CH1 - PD12
+ * 	CH2 - PD13
+ * 	CH3 - PD14
+ * 	CH4 - PD15
+ * TIM15:
+ * 	CH1 - PE5
+ * 	CH2 - PE6
+ */
+
+#define SERVO_DEFAULT_POSITION 150
+#define SERVO_MIN_POSITION 50
+#define SERVO_MAX_POSITION 250
 #define SERVO_DIFF (SERVO_MAX_POSITION - SERVO_DEFAULT_POSITION)
 
 void Servo_init(void);
